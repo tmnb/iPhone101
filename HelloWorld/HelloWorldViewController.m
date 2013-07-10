4150be249a8aea2175cb58be351be517accc5533
Tomonobu Sato <tomonobu.sato@gmail.com>
2013-07-10 21:51:09 +0900

    Finished First iOS Application

--- HelloWorld/HelloWorldViewController.m
+++ HelloWorld/HelloWorldViewController.m
@@ -26,4 +26,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)changeGreeting:(id)sender {
+    self.userName = self.textField.text;
+    
+    NSString *nameString = self.userName;
+    
+    if ([nameString length] == 0) {
+        nameString = @"World";
+    }
+    
+    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!", nameString];
+    
+    self.label.text = greeting;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if (textField == self.textField) {
+        [textField resignFirstResponder];
+    }
+    return YES;
+}
+
 @end
